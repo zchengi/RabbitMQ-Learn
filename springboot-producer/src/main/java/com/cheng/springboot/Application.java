873c652f788e8f722714580bf5a99a325be00c02
cheng <792702352@qq.com>
2018-11-06 23:01:42 +0800

    :sparkles: 消息可靠性配置

--- springboot-producer/src/main/java/com/cheng/springboot/Application.java
+++ springboot-producer/src/main/java/com/cheng/springboot/Application.java
@@ -3,6 +3,10 @@ package com.cheng.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author cheng
+ *         2018/11/06 23:00:17
+ */
 @SpringBootApplication
 public class Application {
 
---
+++ springboot-producer/src/main/java/com/cheng/springboot/MainConfig.java
@@ -0,0 +1,13 @@
+package com.cheng.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cheng
+ *         2018/11/6 22:59
+ */
+@Configuration
+@ComponentScan({"com.cheng.springboot.*"})
+public class MainConfig {
+}
